2808045ad2cf70e5b4775f5ba5681ca6719fcba1
Monir Ahmed <152847216+Monir-cse04@users.noreply.github.com>
2025-12-27 18:15:55 +0600

    Crop_an_image.py

--- cropping_tool.py
+++ cropping_tool.py
@@ -1,32 +1,38 @@
 import cv2
 import numpy as np
-
-#read an image
-flag=False
-ix= -1
-iy= -1
-def draw(event, x, y, flags, param):
+flag= False
+ix=-1
+iy=-1
+def crop(event, x, y, flags, param):
     global ix,iy,flag
     if event == 1:
-        flag=True
+        flag = True
         ix=x
         iy=y
+        
+    # elif event == 0:
+    #     if flag == True:
+    #         cv2.rectangle(img,pt1=(ix,iy),pt2=(x,y),color=(0,0,0),thickness=1)
 
-    elif event == 0:
-        if flag == True:
-            cv2.rectangle(img,pt1=(ix,iy),pt2=(x,y),color=(0,255,0),thickness=-1)
+    elif event==4:
+        fx=x
+        fy=y
+        flag = False
+        cv2.rectangle(img, pt1=(ix,iy), pt2=(x, y), color=(0,0,0), thickness=1)
+        cropped = img[ix:fx, iy:fy] #cropping portion
+        cv2.imshow('crop_img', cropped) #crop pic will show  in new window name crop_img
 
-    elif event == 4:
-        flag=False
-        cv2.rectangle(img,pt1=(x,y),pt2=(x,y),color=(0,255,0),thickness=2)
-
+        #if want to save this cropped picture then use  the followg function
+        # cv2.imwrite('cropped_img.jpg', cropped) 
+        
+        cv2.waitKey(0) #delay
+        
 cv2.namedWindow(winname='img')
-#cv2.setMouseCallback(windowname, function)
-cv2.setMouseCallback('img', draw)
-img= np.zeros((512,512,3))
+cv2.setMouseCallback("img",crop)
+img=cv2.imread("jasmine.png")
 while True:
-    cv2.imshow('img',img)
-    if cv2.waitKey(1) & 0xFF == ord('e'):
-        #to exit from infinite loop press x on KB
+    cv2.imshow("img",img)
+    if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 cv2.destroyAllWindows()
+
